b1eb318dee794693ac073aec4b56dbba7de9af80
Maclenn77 <maclenn77@gmail.com>
2020-06-19 14:43:26 -0500

    Create sessions for the controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :sessions, only: [:create, :new, :destroy]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.input_html
+  get '/sign_up', to: 'users#new'
+  get '/sign_in', to: 'sessions#new'
+  get 'log_out', to: 'sessions#destroy'
 end
