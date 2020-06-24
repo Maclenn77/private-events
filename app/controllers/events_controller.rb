f96ea229fb9471c4250703c42e4fe708cbcde3c7
Maclenn77 <maclenn77@gmail.com>
2020-06-24 14:12:07 -0500

    Add routes to events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,15 +1,21 @@
 # frozen_string_literal: true
 
 class EventsController < ApplicationController
+  include SessionsHelper
+
   def new
     redirect_to root_path if current_user.nil?
     @event = Event.new
   end
 
-  def index; end
+  def index;
+    @events = Event.all
+  end
 
   def create
     @event = current_user.events.build(event_params)
+    @event.save
+    redirect_to root_path
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,16 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.input_html
   get 'sign_up', to: 'users#new'
   get 'show', to: 'users#show'
+
+  #Routes for sessions
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+
+  # Routes for events
+  get 'create_event', to: 'events#new'
+  post 'events', to: 'events#create'
+  get 'events', to: 'events#index'
 
   root 'users#index'
 
