1e677fe1d1cf26a5b8b54b51dfe32f010e3e4194
Pedro Donda <phfdonda@gmail.com>
2020-06-25 16:44:31 -0300

    add toggle function to card

---
+++ app/controllers/invitations_controller.rb
@@ -0,0 +1,5 @@
+class InvitationsController < ApplicationController
+
+# @location = Invitation.address
+
+end
--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -2,6 +2,6 @@ class Invitation < ApplicationRecord
   belongs_to :guests, class_name: 'User'
   belongs_to :event
 
-  scope :address, -> { find_by(id: Invitation.event_id).location }
+  scope :address, -> { }
 
 end
