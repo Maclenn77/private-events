08ea360aaabc62085de75726a1b79897b65492d2
Pedro Donda <phfdonda@gmail.com>
2020-06-26 16:05:30 -0300

    Auto stash before merge of "feature-events" and "origin/feature-events"

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,10 +1,14 @@
 module EventsHelper
   def attending?(event)
+<<<<<<< Updated upstream
 
   end
 
   def creator(event)
     creator = User.find(event.creator_id)
+=======
+    
+>>>>>>> Stashed changes
   end
   
 end
