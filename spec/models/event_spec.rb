702a6cb13c56a2afa818fe5172e3ab0f9cdd7ed7
Pedro Donda <phfdonda@gmail.com>
2020-07-01 14:57:09 -0300

    add scope test

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -7,4 +7,23 @@ RSpec.describe Event do
     it 'should be invalid without parameters' do
       expect(event).to_not be_valid
     end
+  end
+
+  context 'scope tests' do
+    let (:params) { { location: 'Hell', description: 'Take the Highway', creator_id: 1 } }
+
+    it 'a future event shouldn\'t be present in previous events list' do
+      User.new.save
+      events_list = []
+      3.times do
+        e = Event.new(params.merge(date: Time.now))
+        e.save
+        events_list << e
+      end
+      event = Event.new(params.merge(date: '3000-06-30'))
+      event.save
+      previous_events = Event.previous
+      expect(previous_events).to_not include(event)
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
+
 RSpec.describe User do
   context 'after creating a User' do
     it 'name should be unique' do
       @user = User.create(name: 'Tester')
       test_user = User.create(name: 'Tester')
       test_user.valid?
-      assert test_user.errors
-      assert test_user.errors[:name]
+      expect(test_user.errors[:name]).to eql(['has already been taken'])
     end
   end
 end
