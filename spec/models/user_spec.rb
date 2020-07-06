f23c85e0db429ae7ad0b352ae8189eca66150f47
Maclenn77 <maclenn77@gmail.com>
2020-07-06 15:01:40 -0500

    Create test for valid user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User do
+
   context 'after creating a User' do
     let(:user) { create(:random_user) }
+
     it 'name should be unique' do
       test_user = user.dup
       test_user.valid?
       expect(test_user.errors[:name]).to eql(['has already been taken'])
+    end
+
+    it 'create a valid user' do
+        user
+        expect(user).to exist
     end
   end
 end
